45d549cdda05314882ee12e11996514c9bf51c30
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2022-01-19 21:12:58 +0100

    Initial commit

---
+++ src/main/java/pl/lodz/p/ks/it/neighbourlyhelp/NeighbourlyHelpApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.ks.it.neighbourlyhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighbourlyHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighbourlyHelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lodz/p/ks/it/neighbourlyhelp/NeighbourlyHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.ks.it.neighbourlyhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighbourlyHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
