6e744f3c1f3b505d252e49d646dd133ca0c8843b
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2022-01-20 00:11:20 +0100

    New fields added to Account entity, AbstractEntity implementation

--- src/main/java/pl/lodz/p/ks/it/neighbourlyhelp/domain/user/Account.java
+++ src/main/java/pl/lodz/p/ks/it/neighbourlyhelp/domain/user/Account.java
@@ -7,11 +7,15 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import pl.lodz.p.ks.it.neighbourlyhelp.utils.common.AbstractEntity;
+import pl.lodz.p.ks.it.neighbourlyhelp.validator.ContactNumber;
 import pl.lodz.p.ks.it.neighbourlyhelp.validator.Email;
 import pl.lodz.p.ks.it.neighbourlyhelp.validator.Firstname;
+import pl.lodz.p.ks.it.neighbourlyhelp.validator.Language;
 import pl.lodz.p.ks.it.neighbourlyhelp.validator.Lastname;
 import pl.lodz.p.ks.it.neighbourlyhelp.validator.Password;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -21,12 +25,16 @@ import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 
 @Getter
-@Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 @Table(name = Account.TABLE_NAME, indexes = {
@@ -36,7 +44,7 @@ import java.util.Collections;
         @UniqueConstraint(columnNames = {"email"}, name = Account.IX_UQ_EMAIL)
 })
 @Entity
-public class Account implements UserDetails {
+public class Account extends AbstractEntity implements UserDetails {
 
     public static final String TABLE_NAME = "account";
     public static final String IX_ACCOUNT_ID = "ix_account_id";
@@ -53,18 +61,77 @@ public class Account implements UserDetails {
             generator = "account_sequence"
     )
     private Long id;
-    @Firstname
-    private String firstName;
-    @Lastname
-    private String lastName;
+
     @Email
+    @Setter
+    @NotNull
+    @Column(name = "email", nullable = false)
     private String email;
+
+    @Firstname
+    @Setter
+    @NotNull
+    @Column(name = "firstname", nullable = false)
+    private String firstName;
+
+    @Lastname
+    @Setter
+    @NotNull
+    @Column(name = "lastname", nullable = false)
+    private String lastName;
+
     @Password
+    @Setter
+    @NotNull
+    @Column(name = "password", nullable = false, length = 64)
     private String password;
+
+    @Setter
+    @Language
+    @Column(name = "language")
+    private String language;
+
+    @Setter
+    @ContactNumber
+    @Column(name = "contact_number")
+    private String contactNumber;
+
+    @Setter
+    @NotNull
+    @Column(name = "locked", nullable = false)
+    private Boolean locked = false;
+
+    @Setter
+    @NotNull
+    @Column(name = "enabled", nullable = false)
+    private Boolean enabled = false;
+
+    @Setter
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_successful_login_date")
+    private Date lastSuccessfulLoginDate;
+
+    @Setter
+    @Column(name = "last_successful_login_ip_address")
+    private String lastSuccessfulLoginIpAddress;
+
+    @Setter
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_failed_login_date")
+    private Date lastFailedLoginDate;
+
+    @Setter
+    @Column(name = "last_failed_login_ip_address")
+    private String lastFailedLoginIpAddress;
+
+    @Setter
     @Enumerated(EnumType.STRING)
     private AccountRole accountRole;
-    private Boolean locked = false;
-    private Boolean enabled = false;
+
+    @Setter
+    @Min(value = 0)
+    @Column(name = "failed_login_attempts_counter", columnDefinition = "integer default 0")
+    private Integer failedLoginAttemptsCounter = 0;
 
     public Account(String firstName, String lastName, String email, String password, AccountRole accountRole) {
         this.firstName = firstName;
---
+++ src/main/java/pl/lodz/p/ks/it/neighbourlyhelp/utils/common/AbstractEntity.java
@@ -0,0 +1,89 @@
+package pl.lodz.p.ks.it.neighbourlyhelp.utils.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import pl.lodz.p.ks.it.neighbourlyhelp.domain.user.Account;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Getter
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    private Date creationDate;
+
+    @Getter
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modification_date")
+    private Date modificationDate;
+
+    @Getter
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "created_by", updatable = false)
+    private Account createdBy;
+
+    @Getter
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "modified_by")
+    private Account modifiedBy;
+
+    @Getter
+    @Version
+    private Long version;
+
+    @PrePersist
+    private void prePersist() {
+        creationDate = new Date(System.currentTimeMillis());
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        modificationDate = new Date(System.currentTimeMillis());
+    }
+
+    public abstract Long getId();
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (this.getId() != null ? this.getId().hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object.getClass() != this.getClass()) {
+            return false;
+        }
+        AbstractEntity other = (AbstractEntity) object;
+        return (this.getId() != null || other.getId() == null) &&
+                (this.getId() == null || this.getId().equals(other.getId()));
+    }
+
+    @Override
+    public String toString() {
+        ToStringBuilder toStringBuilder = new ToStringBuilder(this)
+                .append("creationDate", creationDate)
+                .append("modificationDate", modificationDate)
+                .append("version", version)
+                .append("createdBy", createdBy == null ? "" : createdBy.getId());
+        if (modifiedBy != null) {
+            toStringBuilder.append("modifiedBy", modifiedBy.getId());
+        }
+        return toStringBuilder.toString();
+    }
+}
