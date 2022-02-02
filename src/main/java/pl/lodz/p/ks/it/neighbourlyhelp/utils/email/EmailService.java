f444fa6be4496f247a0c5935b06179d8642167d8
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2022-02-02 23:28:46 +0100

    Configuration for EmailService added

---
+++ src/main/java/pl/lodz/p/ks/it/neighbourlyhelp/utils/email/EmailService.java
@@ -0,0 +1,11 @@
+package pl.lodz.p.ks.it.neighbourlyhelp.utils.email;
+
+import pl.lodz.p.ks.it.neighbourlyhelp.domain.user.Account;
+import pl.lodz.p.ks.it.neighbourlyhelp.exception.AppBaseException;
+
+public interface EmailService {
+
+    void send(String to, String subject, String content) throws AppBaseException;
+
+    void sendActivationSuccessEmail(Account account) throws AppBaseException;
+}
